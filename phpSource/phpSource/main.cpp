2585cb2a44df7d0cbe7fd9b61d7f9db65dbbfcb9
zongshuai <897579981@qq.com>
2017-02-28 16:25:30 +0800

    新增注释

---
+++ phpSource/phpSource/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  phpSource
+//
+//  Created by zongshuai on 17/2/28.
+//  Copyright © 2017年 zongshuai. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
